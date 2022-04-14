52d298b167f8eb6724d90f6ffd6c541da0854981
Stéphane Alnet <stephane.alnet@signalwire.com>
2022-04-14 07:52:56 -0400

    add branded primivites

--- src/meta-types/primitive.ts
+++ src/meta-types/primitive.ts
@@ -13,14 +13,33 @@ export type Primitive<T extends null | boolean | number | string> =
 export type $Primitive<T> = If<
   A.Equals<T, never>,
   Never,
-  { type: PrimitiveTypeId; value: T }
+  { type: PrimitiveTypeId; value: T; brand: undefined }
+>;
+
+export type BrandedPrimitive<T extends null | boolean | number | string, B extends string> =
+  $BrandedPrimitive<T,B>;
+
+export type $BrandedPrimitive<T, B extends string> = If<
+  A.Equals<T, never>,
+  Never,
+  { type: PrimitiveTypeId; value: T; brand: B }
 >;
 
 export type PrimitiveType = {
   type: PrimitiveTypeId;
   value: null | boolean | number | string;
+  brand: string | undefined;
 };
 
 export type PrimitiveValue<T extends PrimitiveType> = T["value"];
 
-export type ResolvePrimitive<T extends PrimitiveType> = PrimitiveValue<T>;
+export type PrimitiveBrand<T extends PrimitiveType> = T["brand"];
+
+export declare const BrandName: unique symbol;
+
+export interface Branded<B> { [BrandName]: B ; };
+
+export type ResolvePrimitive<T extends PrimitiveType> =
+  PrimitiveBrand<T> extends string
+    ? PrimitiveValue<T> & Branded<PrimitiveBrand<T>>
+    : PrimitiveValue<T>;
